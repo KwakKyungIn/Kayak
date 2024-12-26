16edc4711dba4974a4e91c085ed1e5c55d7e3da4
raEb_kk <b_mule@naver.com>
2024-12-26 17:15:10 +0900

    경로 정리 다시함

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("css"));
-app.use(express.static("image"));
+app.use(express.static("public"));
+
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
 
 app.get("/", (req,res)=>{
     res.render("home/index");
@@ -13,6 +14,12 @@ app.get("/", (req,res)=>{
  
 app.get("/book_page", (req,res)=>{
     res.render("book_page");
+});
+app.get("/product_info_page", (req,res)=>{
+    res.render("product_info_page");
+});
+app.get("/product_list_page", (req,res)=>{
+    res.render("product_list_page");
 });
 
 app.listen(3000,function() {
