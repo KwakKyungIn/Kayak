67d8311997cc9b5b9c0f20989f155b1ec4f65f02
raEb_kk <b_mule@naver.com>
2025-01-03 22:48:01 +0900

    예약관련 사항 완료

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -13,9 +13,18 @@ const product_info_page =(req,res)=>{
 const product_list_page =  (req,res)=>{
     res.render("product_list_page");
 };
+const book_done =  (req,res)=>{
+    res.render("book_done");
+};
+const counsel_book =  (req,res)=>{
+    res.render("counsel_book");
+};
+
 module.exports={
     home,
     book_page,
     product_info_page,
     product_list_page,
+    book_done,
+    counsel_book,
 };
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,5 +8,7 @@ router.get("/", ctrl.home);
 router.get("/book_page", ctrl.book_page);
 router.get("/product_info_page", ctrl.product_info_page);
 router.get("/product_list_page",ctrl.product_list_page);
+router.get("/book_done",ctrl.book_done);
+router.get("/counsel_book",ctrl.counsel_book);
 
 module.exports = router;
